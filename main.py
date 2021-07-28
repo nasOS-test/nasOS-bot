62d01f8a4cdc6cbbbb653c2473138e1976bae2e5
scratcher-402 <75668082+scratcher-402@users.noreply.github.com>
2021-07-28 21:29:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from discord.ext import commands
 from config import settings
 import time
 import json
-from gtts import gTTS as tts
+from gtts import gTTS
+
 import requests
 from pretty_help import DefaultMenu, PrettyHelp
 bot = commands.Bot(command_prefix = settings['prefix'])
@@ -91,7 +92,8 @@ async def avatar(ctx, member: discord.Member):
 async def tts(ctx, text, lang):
  text = str(text)
  lang = str(lang)
- a = tts(text, lang=lang)
+ a = gTTS.tts(text, lang=lang)
+ print(a)
  a.save("aboba.mp3")
  await ctx.send(file=discord.File("aboba.mp3"))
 bot.run(settings['token']) # Обращаемся к словарю settings с ключом token, для получения токена
