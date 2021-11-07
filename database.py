251b7e9fbd40714a14bdc6a2c117782964e74f16
scratcher-402 <75668082+scratcher-402@users.noreply.github.com>
2021-11-07 13:15:08 +0300

    Добавлена поддержка варнов (не полностью, delwarn не дописан)

--- database.py
+++ database.py
@@ -10,6 +10,18 @@ class Rank(db.Model):
     __tablename__ = "ranks"
     userid = db.Column(db.String(30), primary_key=True)
     rank = db.Column(db.Integer())
+class Server(db.Model):
+    __tablename__ = "servers"
+    serverid = db.Column(db.String(30), primary_key=True)
+    prefix = db.Column(db.String(5))
+    adminroleid = db.Column(db.String(30))
+    premium = db.Column(db.Boolean())
+class Warn(db.Model):
+    __tablename__ = "warns"
+    id = db.Column(db.Integer(), primary_key=True)
+    userid = db.Column(db.String(30))
+    serverid = db.Column(db.String(30))
+    text = db.Column(db.Text())
 db.create_all()
 
 def rankup(id):
@@ -31,3 +43,46 @@ def getrank(id):
         return a.rank
     else:
         return 0
+
+def getServerSettings(id):
+    id = str(id)
+    a = db.session.query(Server).get(id)
+    if a:
+        return { "serverID": int(id),
+                 "prefix": a.prefix,
+                 "adminRoleID": a.adminroleid,
+                 "premium": a.premium }
+    else:
+        a = Server(serverid=id, prefix="n!", adminroleid=None, premium=False)
+        return getServerSettings(id)
+
+def getwarns(serverid,userid):
+    lst = db.session.query(Warn).filter(Warn.serverid == str(serverid) and Warn.userid == str(userid)).all()
+    lst2 = []
+    for warn in lst:
+        lst2.append(f"ID: **{warn.id}** \nText: **{warn.text}**")
+    return lst2
+
+def mkwarn(serverid,userid,roleid,txt):
+    ss = getServerSettings(serverid)
+    if ss["adminRoleID"] == roleid:
+        w = Warn(id=db.session.query(Warn).all()[-1].id+1, serverid=serverid, userid=userid, text=txt)
+        db.session.add(w)
+        db.session.commit()
+        return "OK"
+    elif not ss["adminRoleID"]:
+        return "I can't confirm that you are an admin. Server owner must specify admin role using n!set_admin_role <role ID>."
+    else:
+        return "Sorry, you are not an admin."
+
+def SetServerSettings(ss):
+    ss2 = db.session.query(Server).get(ss["serverid"])
+    ss2.prefix = ss["prefix"]
+    ss2.adminroleid = ss["adminRoleID"]
+    ss2.premium = ss["premium"]
+    db.session.add(ss2)
+    db.session.commit()
+
+#def delwarn(id, serverid, roleid):
+#  if getServerSettings(serverid)["adminRoleID"] == roleid:
+    
