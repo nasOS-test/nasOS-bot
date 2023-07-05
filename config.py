a3bf6bb0792f0b794ef17d9e16053b0cbf6acdaa
Egor <petruhin-enot@yandex.ru>
2023-07-05 15:49:29 +0400

    Revert "."
    This reverts commit b24ddeb9110bfe61c35f2ac114579b5b6a3b3f0e.
    
    Возвращен исходный код

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+settings = {
+    'token': os.environ.get('BOT_TOKEN'),
+    'bot': 'nasOS Bot',
+    'id': 806926827552899094,
+    'prefix': 'n!'
+}
---
+++ dbconnect.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine, MetaData, Table, String, Integer, Column, Text, DateTime, Boolean
+from datetime import datetime
+import os
+
+db = create_engine(os.environ.get("DB"))
+db = db.connect()
+metadata = MetaData()
+
+aboba = Table('rank', metadata, 
+    Column('id', Integer(), primary_key=True),
+    Column('rank', Integer()))
+def rankup(id):
+  db.execute(aboba.update().where(aboba.c.id == id).values(rank = db.execute(aboba.select().where(aboba.c.id == id)).fetchone()[1]+1))
+  # db.execute(aboba.insert().values(id=id, rank=1))
+def getrank(id):
+ return(db.execute(aboba.select().where(aboba.c.id == id)).fetchone()[1])
